17818839f70adc75b404aefb39bed65feb207b67
Devanjaneya <147537135+Devanjaneya@users.noreply.github.com>
2025-11-12 19:52:08 +0530

    Implement 'clear' event for clearing canvas
    Added a new event listener for 'clear' to emit 'onclear' to all clients.

--- index.js
+++ index.js
@@ -2,32 +2,41 @@ let express = require("express");
 let app = express();
 let httpServer = require("http").createServer(app);
 let io = require("socket.io")(httpServer);
-let connections=[];
+let connections = [];
+
 io.on("connection", (socket) => {
-    connections.push(socket);
-    console.log("${socket.id} has connected");
+  connections.push(socket);
+  console.log(`${socket.id} has connected`);
 
-    socket.on("draw", (data) => {
-        connections.forEach((con) => {
-            if (con.id !== socket.id) {
-                con.emit("ondraw", {x:data.x, y:data.y});
-            }
-        })
+  socket.on("draw", (data) => {
+    // This code is unchanged. It just passes the new data object (with color/width)
+    connections.forEach((con) => {
+      if (con.id !== socket.id) {
+        con.emit("ondraw", data); // 'data' now contains {x, y, color, width, tool}
+      }
     });
+  });
 
-    socket.on("down", (data) => {
-       connections.forEach((con) => {
-            if (con.id !== socket.id) {
-                con.emit("ondown", {x:data.x, y:data.y});
-            } 
-         });
+  socket.on("down", (data) => {
+    // This code is also unchanged.
+    connections.forEach((con) => {
+      if (con.id !== socket.id) {
+        con.emit("ondown", data); // 'data' now contains {x, y, color, width, tool}
+      }
     });
+  });
 
-    socket.on("disconnect", (reason) => {
-        console.log("${socket.id} has disconnected");
-        connections=connections.filter((con) => con.id !== socket.id);
-    });
-})
+  // NEW: Listen for the "clear" event
+  socket.on("clear", () => {
+    // Emit "onclear" to ALL connected clients (including the sender)
+    io.emit("onclear");
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} has disconnected`);
+    connections = connections.filter((con) => con.id !== socket.id);
+  });
+});
 
 app.use(express.static("public"));
 let PORT = process.env.PORT || 8080;
